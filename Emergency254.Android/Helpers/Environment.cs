4c6537aa2195ba19fbea6d4297c96ce23e88e1d4
davywamahiu <102513090+davywamahiu@users.noreply.github.com>
2022-09-15 09:06:15 +0300

    Add files via upload

---
+++ Emergency254.Android/Helpers/Environment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Content.Res;
+using Android.OS;
+using Emergency254.Interfaces;
+using Emergency254.Models;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Emergency254.Helpers.Environment))]
+namespace Emergency254.Helpers
+{
+    public class Environment : IEnvironment
+    {
+        public void SetStatusBarColor(System.Drawing.Color color, bool darkStatusBarTint)
+        {
+            if (Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.Lollipop)
+                return;
+
+            var activity = Platform.CurrentActivity;
+            var window = activity.Window;
+            window.AddFlags(Android.Views.WindowManagerFlags.DrawsSystemBarBackgrounds);
+            window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+            window.SetStatusBarColor(color.ToPlatformColor());
+            
+            if(Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M)
+            {
+                var flag = (Android.Views.StatusBarVisibility)Android.Views.SystemUiFlags.LightStatusBar;
+                window.DecorView.SystemUiVisibility = darkStatusBarTint ? flag : 0;
+            }
+        }
+    }
+}
